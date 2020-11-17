8a773ae72271a8f5a0b0b504633d607b893dc247
JACKura <292887595@qq.com>
2020-11-17 20:23:25 +0800

    页面修改

--- pages/launch/launch.vue
+++ pages/launch/launch.vue
@@ -6,7 +6,7 @@
 					商品标题
 				</view>
 				<view class="input">
-					<input placeholder="请为此次拼团设置主题" type="text" v-model="name" />
+					<input placeholder="请为此次拼团设置主题" type="text" v-model="name1" />
 				</view>
 			</view>
 			<view class="row">
@@ -16,10 +16,14 @@
 				<!-- <view class="input">
 					<input placeholder="个人拼团/任务拼团" type="text" v-model="name" />
 				</view> -->
-				<select name="" id="select">
-					<option value ="volvo">个人拼团</option>
-					<option value ="saab">任务拼团</option>
-				</select>
+				<radio-group name="radio" class="radio">
+					<label class="label">
+						<radio value="radio1" checked="checked"/><text>个人拼团</text>
+					</label>
+					<label class="label">
+						<radio value="radio2" /><text>任务拼团</text>
+					</label>
+				</radio-group>
 			</view>
 			<view class="row">
 				<view class="nominal">
@@ -36,7 +40,7 @@
 				<view class="input" @tap="chooseCity">
 					{{region.label}}
 				</view>
-				
+
 			</view>
 			<view class="row">
 				<view class="nominal">
@@ -54,17 +58,18 @@
 					<switch color="#f06c7a" :checked="isDefault" @change=isDefaultChange />
 				</view> -->
 				<view>
-					<image id="imageChoose" src="../../static/img/emoji/104.gif" mode="bottom"@tap="choose"></image>
+					<image id="imageChoose" src="../../static/img/emoji/104.gif" mode="bottom" @tap="choose"></image>
 				</view>
-			
+
 			</view>
 		</view>
 		<view class="save" @tap="save">
-			<view class="btn" @click="tiaozhuan">
+			<view class="btn">
 				发布拼团
 			</view>
 		</view>
-		<mpvue-city-picker :themeColor="themeColor" ref="mpvueCityPicker" :pickerValueDefault="cityPickerValue" @onCancel="onCancel" @onConfirm="onConfirm"></mpvue-city-picker>
+		<mpvue-city-picker :themeColor="themeColor" ref="mpvueCityPicker" :pickerValueDefault="cityPickerValue" @onCancel="onCancel"
+		 @onConfirm="onConfirm"></mpvue-city-picker>
 	</view>
 </template>
 
@@ -77,28 +82,29 @@
 		},
 		data() {
 			return {
-				editType:'edit',
-				id:'',
-				name:'',
-				tel:'',
-				detailed:'',
-				isDefault:false,
+				editType: 'edit',
+				id: '',
+				name: '',
+				tel: '',
+				detailed: '',
+				isDefault: false,
 				cityPickerValue: [0, 0, 1],
 				themeColor: '#007AFF',
-				region:{label:"请点击选择地址",value:[],cityCode:""}
+				region: {
+					label: "请点击选择地址",
+					value: [],
+					cityCode: ""
+				}
 			};
 		},
 		methods: {
-			tiaozhuan(){
-				uni.navigateTo({
-					url:"./success/success"
-				})
-			},
-			choose(){
-				wx.chooseImage({success: function(res) {
-					console.log(res.tempFilePaths)
-					// document.getElementById("imageChoose").
-				},});
+			choose() {
+				wx.chooseImage({
+					success: function(res) {
+						console.log(res.tempFilePaths)
+						// document.getElementById("imageChoose").
+					},
+				});
 			},
 			onCancel(e) {
 				console.log(e)
@@ -110,60 +116,85 @@
 				this.region = e;
 				this.cityPickerValue = e.value;
 			},
-			isDefaultChange(e){
+			isDefaultChange(e) {
 				this.isDefault = e.detail.value;
 			},
-			
-			save(){
-				let data={"name":this.name,"head":this.name.substr(0,1),"name1":this.name1,"tel":this.tel,"address":{"region":this.region,"detailed":this.detailed},"isDefault":this.isDefault}
-				if(this.editType=='edit'){
+
+			save() {
+				let data = {
+					"name": this.name,
+					"head": this.name.substr(0, 1),
+					"name1": this.name1,
+					"tel": this.tel,
+					"address": {
+						"region": this.region,
+						"detailed": this.detailed
+					},
+					"isDefault": this.isDefault
+				}
+				if (this.editType == 'edit') {
 					data.id = this.id
 				}
-				if(!data.name1){
-					uni.showToast({title:'请输入拼团主题',icon:'none'});
-					return ;
+				if (!data.name1) {
+					uni.showToast({
+						title: '请输入拼团标题',
+						icon: 'none'
+					});
+					return;
 				}
 				// if(!data.name){
 				// 	uni.showToast({title:'请选择拼团类型',icon:'none'});
 				// 	return ;
 				// }
-				if(!data.tel){
-					uni.showToast({title:'请输入拼团相关信息',icon:'none'});
-					return ;
+				if (!data.tel) {
+					uni.showToast({
+						title: '请输入拼团相关信息',
+						icon: 'none'
+					});
+					return;
 				}
-				if(!data.address.detailed){
-					uni.showToast({title:'请输入商家详细地址',icon:'none'});
-					return ;
+				if (!data.address.detailed) {
+					uni.showToast({
+						title: '请输入商家详细地址',
+						icon: 'none'
+					});
+					return;
 				}
-				if(data.address.region.value.length==0){
-					uni.showToast({title:'请选择商家所在地区',icon:'none'});
-					return ;
+				if (data.address.region.value.length == 0) {
+					uni.showToast({
+						title: '请选择商家所在地区',
+						icon: 'none'
+					});
+					return;
 				}
 				uni.showLoading({
-					title:'正在提交'
+					title: '正在提交'
 				})
 				//实际应用中请提交ajax,模板定时器模拟提交效果
-				setTimeout(()=>{
+				setTimeout(() => {
 					uni.setStorage({
-						key:'saveAddress',
-						data:data,
+						key: 'saveAddress',
+						data: data,
 						success() {
 							uni.hideLoading();
+							uni.navigateTo({
+								url: "./success/success"
+							})
 							// uni.navigateBack();
 						}
 					})
-				},300)
-				
-				
+				}, 300)
+
+
 			}
 		},
 		onLoad(e) {
 			//获取传递过来的参数
-			
+
 			this.editType = e.type;
-			if(e.type=='edit'){
+			if (e.type == 'edit') {
 				uni.getStorage({
-					key:'address',
+					key: 'address',
 					success: (e) => {
 						this.id = e.data.id;
 						this.name = e.data.name;
@@ -175,7 +206,7 @@
 					}
 				})
 			}
-			
+
 		},
 		onBackPress() {
 			if (this.$refs.mpvueCityPicker.showPicker) {
@@ -191,11 +222,11 @@
 	};
 </script>
 <style lang="scss">
-
-.save{
-		view{
+	.save {
+		view {
 			display: flex;
 		}
+
 		position: fixed;
 		bottom: 0;
 		width: 100%;
@@ -203,8 +234,9 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		.btn{
-			box-shadow: 0upx 5upx 10upx rgba(0,0,0,0.4);
+
+		.btn {
+			box-shadow: 0upx 5upx 10upx rgba(0, 0, 0, 0.4);
 			width: 70%;
 			height: 80upx;
 			border-radius: 80upx;
@@ -212,60 +244,73 @@
 			color: #fff;
 			justify-content: center;
 			align-items: center;
-			.icon{
+
+			.icon {
 				height: 80upx;
 				color: #fff;
 				font-size: 30upx;
 				justify-content: center;
 				align-items: center;
 			}
+
 			font-size: 30upx;
 		}
 	}
-	.content{
+
+	.content {
 		display: flex;
 		flex-wrap: wrap;
-		view{
+
+		view {
 			display: flex;
 		}
-		.row{
+
+		.row {
 			width: 94%;
 			margin: 0 3%;
 			border-top: solid 1upx #eee;
-			#select{
-				border: none;
+
+			.radio{
+				margin-top: 35rpx;
+				.label{
+					margin-right: 10rpx;
+				}
 			}
-			.nominal{
+
+			.nominal {
 				width: 30%;
 				height: 120upx;
 				font-weight: 200;
 				font-size: 30upx;
 				align-items: center;
 			}
-			.input{
+
+			.input {
 				width: 70%;
 				padding: 20upx 0;
 				align-items: center;
 				font-size: 30upx;
-				&.switch{
+
+				&.switch {
 					justify-content: flex-end;
 				}
-				.textarea{
+
+				.textarea {
 					margin: 20upx 0;
 					min-height: 120upx;
 				}
 			}
-			.del{
+
+			.del {
 				width: 100%;
 				height: 100upx;
 				justify-content: center;
 				align-items: center;
 				font-size: 36upx;
 				color: #595BB3;
-				background-color: rgba(255,0,0,0.05);
+				background-color: rgba(255, 0, 0, 0.05);
 				border-bottom: solid 1upx #eee;
 			}
 		}
 	}
-	
 </style>
